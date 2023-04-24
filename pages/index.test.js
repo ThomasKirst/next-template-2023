6e7693b26f95f1040cf5d01c75d9f57ce747f729
Thomas Kirst <thomas.kirst@gmail.com>
2023-04-24 17:25:12 +0200

    Adjust test subject

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import HomePage from "../pages/index";
 
-test("renders learn next headline", () => {
+test("renders next greeting headline", () => {
   render(<HomePage />);
   const linkElement = screen.getByText(/hello from next.js/i);
   expect(linkElement).toBeInTheDocument();
